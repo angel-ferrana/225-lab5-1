ca2968433c8ded22ac99bbb368ff866e466dd3eb
angel-ferrana <ferrana@miamioh.edu>
2025-12-11 22:13:51 -0500

    Update data-gen.py

--- data-gen.py
+++ data-gen.py
@@ -1,5 +1,5 @@
 import sqlite3
-import os
+import random
 
 DATABASE = '/nfs/demo.db'
 
@@ -7,16 +7,22 @@ def connect_db():
     """Connect to the SQLite database."""
     return sqlite3.connect(DATABASE)
 
-def generate_test_data(num_contacts):
-    """Generate test data for the contacts table."""
+def generate_test_data(num_books):
+    """Generate test data for the books table."""
     db = connect_db()
-    for i in range(num_contacts):
-        name = f'Test Name {i}'
-        phone = f'123-456-789{i}'
-        db.execute('INSERT INTO contacts (name, phone) VALUES (?, ?)', (name, phone))
+
+    for i in range(num_books):
+        title = f'Test Book {i}'
+        price = round(random.uniform(5.0, 50.0), 2)
+
+        db.execute(
+            'INSERT INTO books (title, price) VALUES (?, ?)',
+            (title, price)
+        )
+
     db.commit()
-    print(f'{num_contacts} test contacts added to the database.')
+    print(f'{num_books} test books added to the database.')
     db.close()
 
 if __name__ == '__main__':
-    generate_test_data(10)  # Generate 10 test contacts.
+    generate_test_data(10)  # Generate 10 test books
